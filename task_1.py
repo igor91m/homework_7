bb507aac6f80cd02ec87b9393f203d3bd1344a24
igor91m <igor91m@yandex.ru>
2021-02-13 16:41:17 +0300

    add changes

--- task_1.py
+++ task_1.py
@@ -13,6 +13,7 @@
 '''
 
 class Matrix:
+
     def __init__(self, list_1, list_2):
         # self.matr = [list_1, list_2]
         self.list_1 = list_1
@@ -22,19 +23,13 @@ class Matrix:
         matr = [[0, 0, 0],
                 [0, 0, 0],
                 [0, 0, 0]]
-
         for i in range(len(self.list_1)):
-
             for j in range(len(self.list_2[i])):
                 matr[i][j] = self.list_1[i][j] + self.list_2[i][j]
-
         return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
-
 
     def __str__(self):
         return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
-
-
 
 my_matrix = Matrix([[7, 15, 12],
                     [5, 19, 21],
